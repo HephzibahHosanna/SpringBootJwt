c80baf35ccc4389645a5ba25f131bb6858069dd9
HephzibahHosanna <hephzibah.hosanna@dyooti.com>
2023-05-08 21:53:03 +0530

    first commit

---
+++ src/main/java/com/example/SpringBootJwt/Dto/AuthenticationResponseDto.java
@@ -0,0 +1,17 @@
+package com.example.SpringBootJwt.Dto;
+
+import java.io.Serializable;
+
+public class AuthenticationResponseDto implements Serializable {
+
+    private static final long serialVersionUID = -8091879091924046844L;
+    private final String jwttoken;
+    public AuthenticationResponseDto(String jwttoken) {
+        this.jwttoken = jwttoken;
+    }
+
+    public String getToken() {
+        return this.jwttoken;
+    }
+
+}
